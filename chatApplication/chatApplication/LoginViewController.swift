7ac5ace67ea243aae71eca313bc9b209905e75de
Nagy Andras <andras1530@gmail.com>
2024-01-16 11:40:11 +0200

    show notification for data input
    if there are missing fields when clicking login or forgot pass, notify the user
    fixing SDK does not contain libarclite at the path

--- chatApplication/chatApplication/LoginViewController.swift
+++ chatApplication/chatApplication/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import ProgressHUD
 
 class LoginViewController: UIViewController {
     
@@ -40,17 +41,36 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
         updateUIFor(login: true)
         setupTextFieldDeletages()
+        setupBackgroundTap()
     }
     
     //Mark: IBActions
     
     @IBAction func loginButtonPressed(_ sender: Any) {
+        if isDataInputedFor(type: isLogin ? "login" : "register") {
+            //login or register
+            print("have data for login/register")
+        } else {
+            ProgressHUD.failed("All fields are required")
+        }
     }
     
     @IBAction func forgotPasswordButtonPressed(_ sender: Any) {
+        if isDataInputedFor(type: "password") {
+            //reset password
+            print("have data for reset pass")
+        } else {
+            ProgressHUD.failed("Email is required.")
+        }
     }
     
     @IBAction func resendEmailButtonPressed(_ sender: Any) {
+        if isDataInputedFor(type: "password") {
+            //resend Verification email
+            print("have data for resend email")
+        } else {
+            ProgressHUD.failed("Email is required.")
+        }
     }
     
     @IBAction func signUpButtonPressed(_ sender: UIButton) {
@@ -68,6 +88,16 @@ class LoginViewController: UIViewController {
     
     @objc func textFieldDidChange(_ textfield: UITextField) {
         updatePlaceholderLabels(textfield: textfield)
+    }
+    
+    @objc func setupBackgroundTap() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(backgroundTap))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func backgroundTap() {
+        print("background tap")
+        view.endEditing(false)
     }
     
     //Mark: -Animation
@@ -94,6 +124,20 @@ class LoginViewController: UIViewController {
             repeatPasswordLabel.text = textfield.hasText ? "Repeat Password" : ""
         }
     }
+    
+    //Mark: -Helpers
+    private func isDataInputedFor(type: String) -> Bool {
+        switch type {
+            case "login":
+                return emailTextField.text != "" && passwordTextField.text != ""
+            case "registration":
+                return emailTextField.text != "" && passwordTextField.text != "" && repeatTextField.text != ""
+            default:
+                return emailTextField.text != ""
+        }
+    }
+    
+    
     
     
 }
